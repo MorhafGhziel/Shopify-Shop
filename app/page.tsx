038b0be578ef043fbe2b7a806dd4d324baa5e25a
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-19 15:52:37 +0300

    created and imported the header section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "@/components/Header";
 
 const page = () => {
   return (
-    <div>
-      <p>page</p>
-    </div>
+    <>
+      <Header />
+    </>
   );
 };
 
