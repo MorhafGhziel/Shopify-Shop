fc6a350ed1114021a3637d5a003d56822e77b565
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-19 16:01:58 +0300

    added the header bg

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,15 @@
+"use client";
+
 import React from "react";
+import Image from "next/image";
+import HeaderBg from "../public/Header-bg.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="">
+      <Image src={HeaderBg} alt="header bg" />
+    </header>
+  );
 };
 
 export default Header;
